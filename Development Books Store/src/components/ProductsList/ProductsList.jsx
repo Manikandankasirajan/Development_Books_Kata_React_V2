dd43b35d54c821b4b7d8ea91f46d809363601599
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-16 21:14:41 +0530

    chore : add raw data to display book info in the app

--- Development Books Store/src/components/ProductsList/ProductsList.jsx
+++ Development Books Store/src/components/ProductsList/ProductsList.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { BOOK_LIST } from "../../data/constants";
+import Product from "../Product/Product";
 
 const ProductsList = () => {
 	return (
 		<>
 			<h2>Books Available</h2>
+			<div className="container">
+				<ul className="productsContainer">
+					{BOOK_LIST.map((book) => {
+						return (
+							<li key={book.id}>
+								<Product book={book} />
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</>
 	);
 };
---
+++ Development Books Store/src/data/constants.js
@@ -0,0 +1,43 @@
+export const BOOK_LIST = [
+	{
+		id: 1,
+		title: "Clean Code",
+		author: "Robert Martin",
+		releaseYear: 2008,
+		price: 50,
+		imgSrc: "../src/assets/Kata_DevelopmentBooks_CleanCode.png",
+	},
+	{
+		id: 2,
+		title: "The Clean Coder",
+		author: "Robert Martin",
+		releaseYear: 2011,
+		price: 50,
+		imgSrc: "../src/assets/Kata_DevelopmentBooks_CleanCoder.png",
+	},
+	{
+		id: 3,
+		title: "Clean Architecture",
+		author: "Robert Martin",
+		releaseYear: 2017,
+		price: 50,
+		imgSrc: "../src/assets/Kata_DevelopmentBooks_CleanArchitecture.jpeg",
+	},
+
+	{
+		id: 4,
+		title: "Test Driven Development by Example",
+		author: "Kent Beck",
+		releaseYear: 2003,
+		price: 50,
+		imgSrc: "../src/assets/Kata_DevelopmentBooks_TDD.jpeg",
+	},
+	{
+		id: 5,
+		title: "Working Effectively With Legacy Code",
+		author: "Michael C. Feathers",
+		releaseYear: 2004,
+		price: 50,
+		imgSrc: "../src/assets/Kata_DevelopmentBooks_Refactoring.jpeg",
+	},
+];
